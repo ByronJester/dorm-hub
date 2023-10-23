20af5d64a68ff11800826905ba89435a2284415a
dormhub <balayandomhub@gmail.com>
2023-10-23 10:09:13 +0800

    commit

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -257,7 +257,7 @@ export default {
                         sm:grid-cols-2
                         md:grid-cols-3
                         lg:grid-cols-4
-                        xl:grid-cols-6
+                        xl:grid-cols-5
                         2xl:grid-cols-6
                         gap-8
                     "
