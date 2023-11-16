5dda319f2e33e63bb459543334a932cd38658490
dormhub <balayandomhub@gmail.com>
2023-11-16 15:25:15 +0800

    'commit'

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,33 +17,43 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'first_name' => 'Admin',
-            'last_name' => 'Admin',
-            'phone_number' => '0977000000',
+            'first_name' => 'Luigi',
+            'last_name' => 'Butor',
+            'phone_number' => '09616019985',
             'password' => Hash::make('password'),
-            'username' => 'admin1',
+            'username' => '@admin.luigi',
             'user_type' => 'admin',
             'first_logged_in' => false
         ]);
 
 
         User::create([
-            'first_name' => 'Admin',
-            'last_name' => 'Admin',
-            'phone_number' => '0977000000',
+            'first_name' => 'Gene Mari Christopher',
+            'last_name' => 'De La Rea',
+            'phone_number' => '09165984951',
             'password' => Hash::make('password'),
-            'username' => 'admin2',
+            'username' => '@admin.jear',
             'user_type' => 'admin',
             'first_logged_in' => false
         ]);
 
 
         User::create([
-            'first_name' => 'Admin',
-            'last_name' => 'Admin',
+            'first_name' => 'Giovanni',
+            'last_name' => 'Marco',
             'phone_number' => '0977000000',
             'password' => Hash::make('password'),
-            'username' => 'admin3',
+            'username' => '@admin.gio',
+            'user_type' => 'admin',
+            'first_logged_in' => false
+        ]);
+
+        User::create([
+            'first_name' => 'Michael',
+            'last_name' => 'Lapuz',
+            'phone_number' => '0977000000',
+            'password' => Hash::make('password'),
+            'username' => '@admin.michael',
             'user_type' => 'admin',
             'first_logged_in' => false
         ]);
@@ -53,7 +63,7 @@ class UserSeeder extends Seeder
             'last_name' => 'Admin',
             'phone_number' => '0977000000',
             'password' => Hash::make('password'),
-            'username' => 'admin4',
+            'username' => 'admin',
             'user_type' => 'admin',
             'first_logged_in' => false
         ]);
