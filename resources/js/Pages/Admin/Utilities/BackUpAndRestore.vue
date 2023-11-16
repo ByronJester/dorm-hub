3b42925ecc995f49d0bb0e4ed156eedebe21e8bf
Byron Jester Manalo <byronjester.manalo@gmail.com>
2023-11-17 03:04:06 +0800

    Restore Modal

--- resources/js/Pages/Admin/Utilities/BackUpAndRestore.vue
+++ resources/js/Pages/Admin/Utilities/BackUpAndRestore.vue
@@ -55,6 +55,17 @@ export default {
                 }
             })
         },
+        restore() {
+            var modal = document.getElementById("restoreModal");
+
+            modal.style.display = "block";
+        },
+        closeRestoreModal() {
+            var modal = document.getElementById("restoreModal");
+
+            modal.style.display = "none";
+        }
+
     }
 }
 </script>
@@ -68,12 +79,87 @@ export default {
 
                 <hr class="h-px my-5 bg-orange-400 border-1 dark:bg-gray-700" />
                 <div class="bg-white p-5 rounded-md shadow">
-                    <button class="py-2 px-3 bg-orange-400 text-white rounded" v-on:click="backup">
+                    <button class="py-2 px-3 bg-orange-400 text-white rounded mr-2" v-on:click="backup">
                         Start Back-Up
+                    </button>
+                    <button class="py-2 px-3 bg-cyan-400 text-white rounded" @click="restore">
+                        Restore
                     </button>
                 </div>
         </div>
-        <div class="w-full mb-5 mt-5">
+
+                <div
+                    id="restoreModal"
+                    tabindex="-1"
+                    aria-hidden="true"
+                    style="background-color: rgba(0, 0, 0, 0.7)"
+                    class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+                >
+                    <div class="h-screen flex justify-center items-center">
+                        <div class="relative w-full max-w-md max-h-full">
+                            <!-- Modal content -->
+                            <div class="relative bg-white rounded-lg shadow">
+                                <!-- Modal header -->
+                                <div
+                                    class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600"
+                                >
+                                    <h3
+                                        class="text-xl font-semibold text-black"
+                                    >
+                                        Restore
+                                    </h3>
+                                    <button
+                                        type="button"
+                                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                        @click="closeRestoreModal()"
+                                    >
+                                        <svg
+                                            class="w-3 h-3"
+                                            aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 14 14"
+                                        >
+                                            <path
+                                                stroke="currentColor"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                            />
+                                        </svg>
+                                        <span class="sr-only">Close modal</span>
+                                    </button>
+                                </div>
+                                <!-- Modal body -->
+                                <div class="p-6 space-y-6">
+                                    <input type="file" />
+                                </div>
+
+                                <!-- Modal footer -->
+                                <div
+                                    class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600"
+                                >
+                                    <button
+                                        type="button"
+                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    >
+                                        Submit
+                                    </button>
+                                    <button
+                                        @click="closeRestoreModal()"
+                                        type="button"
+                                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="w-full mb-5 mt-5">
                     <div
                         class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white border"
                     >
