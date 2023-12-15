33e21e4c271deed4ad41d7de2b14b8b07efde512
dormhub <balayandomhub@gmail.com>
2023-12-15 14:49:59 +0800

    size

--- resources/js/Pages/Tenant/Dorms.vue
+++ resources/js/Pages/Tenant/Dorms.vue
@@ -146,10 +146,10 @@ export default {
                             grid
                             grid-cols-1
                             sm:grid-cols-2
-                            md:grid-cols-3
-                            lg:grid-cols-3
-                            xl:grid-cols-4
-                            2xl:grid-cols-4
+                            md:grid-cols-2
+                            lg:grid-cols-2
+                            xl:grid-cols-3
+                            2xl:grid-cols-3
                             gap-8
                         "
                 >
