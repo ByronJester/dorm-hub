b731383f07119ab4e26094729f613de280b498d6
dormhub <balayandomhub@gmail.com>
2023-12-16 08:11:30 +0800

    origin

--- resources/js/Pages/Owner/ApplicationModule.vue
+++ resources/js/Pages/Owner/ApplicationModule.vue
@@ -491,7 +491,7 @@
                                         Decline
                                     </button>
                                     <button
-                                        @click="approveApplication(selectedApplication.room_id)"
+                                        @click="approveApplication(selectedApplication)"
                                         type="button"
                                         v-if="selectedApplication && selectedApplication.status == 'pending' && !selectedApplication.is_approved"
                                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
