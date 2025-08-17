b63790e5e8f52fa37f71592fe359eed3f53c8e3c
Byron Jester Manalo <36653627+ByronJester@users.noreply.github.com>
2025-08-18 01:14:09 +0800

    Update entrypoint.sh seed

--- deployment/octane/entrypoint.sh
+++ deployment/octane/entrypoint.sh
@@ -14,7 +14,7 @@ initialStuff() {
     php artisan event:cache; \
     php artisan config:cache; \
     php artisan route:cache; \
-    php artisan migrate;
+    php artisan migrate --seed;
 }
 
 if [ "$1" != "" ]; then
